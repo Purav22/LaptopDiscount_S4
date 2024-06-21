421d8fd08e315285c93e5ba218713084276ac286
Purav22 <ppp22112000@gmail.com>
2024-06-21 13:20:38 -0400

    Unit Testing completed

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -3,22 +3,85 @@ using LaptopDiscount;
 namespace TestProject1
 {
 
-
     [TestFixture]
-    public class EmployeeDiscountTests
+    public class Test
     {
         [Test]
-        public void CalculateDiscountedPrice_PartTime_NoDiscount()
+        public void CalculatePrice_PartTime()
+        {
+            var discountPrice = new EmployeeDiscount
+            {
+                EmployeeType = EmployeeType.PartTime,
+                Price = 300 
+            };
+
+            decimal discountedPrice = discountPrice.CalculateDiscountedPrice();
+            Assert.AreEqual(300, discountedPrice); 
+        }
+        [Test]
+        public void CalculateDiscountedPrice_5Per()
         {
             var discount = new EmployeeDiscount
             {
-                EmployeeType = EmployeeType.PartTime,
-                Price = 100 // Example price
+                EmployeeType = EmployeeType.PartialLoad,
+                Price = 200
             };
 
             decimal discountedPrice = discount.CalculateDiscountedPrice();
-            Assert.AreEqual(100, discountedPrice); // No discount expected
+            Assert.AreEqual(190, discountedPrice); 
         }
+
+        [Test]
+        public void CalculateDiscountedPrice_10PerFullTime()
+        {
+            var discount = new EmployeeDiscount
+            {
+                EmployeeType = EmployeeType.FullTime,
+                Price = 200
+            };
+
+            decimal discountedPrice = discount.CalculateDiscountedPrice();
+            Assert.AreEqual(180, discountedPrice); 
+        }
+
+        [Test]
+        public void CalculateDiscountedPrice_20Per()
+        {
+            var discount = new EmployeeDiscount
+            {
+                EmployeeType = EmployeeType.CompanyPurchasing,
+                Price = 200
+            };
+
+            decimal discountedPrice = discount.CalculateDiscountedPrice();
+            Assert.AreEqual(160, discountedPrice); 
+        }
+
+        [Test]
+        public void CalculateDiscountedPrice_NegativeValue()
+        {
+            var discount = new EmployeeDiscount
+            {
+                EmployeeType = EmployeeType.FullTime,
+                Price = -10
+            };
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => discount.CalculateDiscountedPrice());
+        }
+
+        [Test]
+        public void CalculateDiscountedPrice_LowPrice()
+        {
+            var discount = new EmployeeDiscount
+            {
+                EmployeeType = EmployeeType.CompanyPurchasing,
+                Price = 10 
+            };
+
+            decimal discountedPrice = discount.CalculateDiscountedPrice();
+            Assert.AreEqual(8, discountedPrice); 
+        }
+
 
     }
 }
